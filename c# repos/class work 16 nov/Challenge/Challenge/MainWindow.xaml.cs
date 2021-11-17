c7f6fdb4780b69572c63b8f6e5f449c23eaad156
MMHEW <92035863+MMHEW@users.noreply.github.com>
2021-11-17 11:56:46 -0500

    commit

--- c# repos/class work 16 nov/Challenge/Challenge/MainWindow.xaml.cs
+++ c# repos/class work 16 nov/Challenge/Challenge/MainWindow.xaml.cs
@@ -26,26 +26,106 @@ namespace Challenge
             InitializeComponent();
         }
 
-        public void addSize(object a, EventArgs e)
+
+        public string size = "small";
+        public string sugar;
+        public string cream;
+        public string type = "frappe";
+        public string order;
+
+        public string with;
+        public string and;
+        public bool _cream;
+        public bool _sugar;
+
+        public void sendOrder(object a, EventArgs e)
+        {
+            if(_cream && _sugar)
+            {
+                with = " with ";
+                and = " and ";
+            }
+            else if (_cream || _sugar)
+            {
+                with = " with ";
+                and = "";
+            }
+            else
+            {
+                with = "";
+                and = "";
+            }
+
+            order = (size + " " + type + with + cream + and + sugar);
+            Debug.WriteLine("Button was pressed");
+            Block.Text = order;
+        }
+
+        public void typeChosen(object a, EventArgs e)
+        {
+            Debug.WriteLine("Combo Box Pushed");
+
+           type =  Combo.SelectedValue.ToString();
+        }
+
+
+
+        public void addSmall(object a, EventArgs e)
         {
 
             Debug.WriteLineIf(true, "Radio Buttons Pushed");
+            size = "small";
         }
 
-       public void addCondiments (object a, EventArgs e)
+        public void addLarge(object a, EventArgs e)
+        {
+
+            Debug.WriteLineIf(true, "Radio Buttons Pushed");
+            size = "Large";
+        }
+
+        public void addMedium(object a, EventArgs e)
+        {
+
+            Debug.WriteLineIf(true, "Radio Buttons Pushed");
+            size = "Medium";
+        }
+
+        public void addSugar (object a, EventArgs e)
         {
             Debug.WriteLineIf(true,"Checkbox Buttons Pushed");
 
+            if (Sugar.IsChecked == true)
+            {
+                sugar = "Sugar";
+                _sugar = true;
+            }
+            else
+            {
+                sugar = "";
+                _sugar = false;
+            }
         }
 
-        public void typeChosen (object a, EventArgs e)
+        public void addCream(object a, EventArgs e)
         {
-            Debug.WriteLine("Combo Box Pushed");
-        }
-       
-        private void CheckBox_Click(object sender, RoutedEventArgs e)
-        {
+            if (Cream.IsChecked == true)
+            {
+                cream = "Cream";
+                _cream = true;
+            }
 
+            else
+            {
+                cream = "";
+                _cream = false;
+            }
+            
+        }
+
+        private void addExtraLarge(object sender, EventArgs e)
+        {
+            size = "Big Chungus";
         }
     }
 }
