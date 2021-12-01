8ac44ba908a9d06d11cac73acf06b2d3aa19cb81
MMHEW <92035863+MMHEW@users.noreply.github.com>
2021-11-30 20:20:50 -0500

    commit 4

---
+++ c# repos/class work 23 nov/HighestScoringWord/HighestScoringWord/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HighestScoringWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(High("aa b"));
+            Console.WriteLine(High("man i need a taxi up to ubud"));
+        }
+
+        public static string High(string s)
+        {
+            string[] words = s.Split(' ');
+
+            int[] values = new int[words.Length];
+
+            for (int i = 0; i < words.Length; ++i)
+            {
+
+                for (int j = 0; j < words[i].Length; ++j)
+                {
+
+                    if (j == 0) values[i] = words[i][j];
+                    else  values[i] += words[i][j];
+
+                }
+
+            }
+
+            int returnValue = 0;
+            for (int i = 0; i < values.Length - 1; ++i)
+            {
+                if (values[i] > values[i + 1]) returnValue = i;
+                else returnValue = (i + 1);
+
+            }
+
+            return words[returnValue];
+        }
+    }
+}
