02f21c535216b930048faef0ffbc4b2635b4696e
MMHEW <92035863+MMHEW@users.noreply.github.com>
2022-02-15 11:20:04 -0500

    Finished classwork repo

---
+++ Azure'/Auth/az204-auth/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "ab05d80f-e6d5-4f1d-9c8d-61d1d2022927";
+        private const string _tenantId = "54b252d8-46e8-4251-84b4-1073ce200816";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build(); 
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
